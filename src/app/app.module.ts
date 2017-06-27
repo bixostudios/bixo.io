34154ee519de9e12b3b59c76e1c88b425bbd10f3
nikvicros <nikho.rosales@gmail.com>
2017-06-27 10:30:03 -0500

    Additional Scaffolding Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './ui-components/navigation/navigation.component';
+import { FooterComponent } from './ui-components/footer/footer.component';
+import { SectionHeroComponent } from './ui-components/section-hero/section-hero.component';
+import { CallToActionComponent } from './ui-components/call-to-action/call-to-action.component';
+import { AboutUsComponent } from './ui-components/about-us/about-us.component';
+import { SectionServicesComponent } from './ui-components/section-services/section-services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    SectionHeroComponent,
+    CallToActionComponent,
+    AboutUsComponent,
+    SectionServicesComponent
   ],
   imports: [
     BrowserModule,
